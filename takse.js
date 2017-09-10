04a0457076e1afc9b228408bfa8ec84bf1137de2
Sakie90 <sara_stevic@yahoo.com>
2017-09-11 00:00:40 +0200

    Add files via upload

---
+++ takse.js
@@ -0,0 +1,33 @@
+// var cene = [5, 145, 69, 56, 13, 456, 78, 4, 30]
+// var taksa = 0
+
+// for (var i = 0; i >= 50; i++) {
+// 	if (cene[i]>taksa) {
+//         var taksa=cene[i];
+//     }
+// }
+
+// console.log(i);
+
+
+
+
+
+
+
+var cene = [5, 145, 69, 56, 13, 456, 78, 4, 30];
+
+for (i = 0; i>= 50; i++) {
+    console.log(i + i* 0.08);
+} 
+
+
+for (i = 0; i<= 50; i++) {
+    console.log(i + i* 0.18);
+} 
+
+
+
+
+
+
